6c856d2cf89deaf13a7bf067f1827703a5e05ff1
CallMeAL <alexpgarner@gmail.com>
2023-02-14 01:50:57 -0800

    2/13 codeawarriors

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -94,3 +94,20 @@
 // {
 //   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
 // }
+// Array Mappings
+// Oh no, the map method for arrays has been disabled. Can you fix it?
+
+// In case you haven't come across the map method for arrays, here is how it works:
+
+// [1,2,3].map(x => x ** 2) === [1,4,9]
+// [1,2,3].map(x => 2 * x) === [2,4,6]
+// [1,2,3].map(x => 2 ** x) === [2,4,8]
+// [1,2,3].map(x => x.toString()) === ["1","2","3"]
+// ["1","2","3"].map(x => parseInt(x)) === [1,2,3]
+// ["random","array","to","be","mapped"].map(x => mapping(x)) === [mapping("random"),mapping("array"),mapping("to"),mapping("be"),mapping("mapped")]
+
+// Array.prototype.map = function(callback){
+//   let newArr = [];
+//   this.forEach((value) => newArr.push(callback(value)));
+//   return newArr;
+// }
