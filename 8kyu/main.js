5b92046c811235277efa43d98abb26bac876a47f
CallMeAL <alexpgarner@gmail.com>
2023-02-20 23:54:40 -0800

    8kyu for the push

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -59,3 +59,17 @@
 //     }
 //     return reverse;
 //   }
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// function repeatStr (n, s) {
+//     let repeat=''
+//     for(let i=0;i<n;i++){
+//       repeat += s;
+//     }
+//     return repeat;
+//   }
