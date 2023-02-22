ea7d0c38498e6255a60f815d3454dd73fe0118d7
CallMeAL <alexpgarner@gmail.com>
2023-02-21 23:33:08 -0800

    easy

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -73,3 +73,11 @@
 //     }
 //     return repeat;
 //   }
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// function booleanToString(b){
+//     return `${b}`
+//   }
