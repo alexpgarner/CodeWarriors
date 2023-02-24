4e610bcad03c360694d9cf1ec332ed29cf1a1a11
CallMeAL <alexpgarner@gmail.com>
2023-02-23 21:08:33 -0800

    binary tree 6kyu. not a lot solutions and recursive ones hard to read. Go back for better answers later

--- 6kyu/main.js
+++ 6kyu/main.js
@@ -415,3 +415,66 @@
 //     let d = Math.log(b2) * e2 - Math.log(b1) * e1;
 //     return (d > 0) - (d < 0);
 //   }
+// We are storing numbers in the nodes of a binary tree. The tree starts at the root node. The root has two child nodes, its leftchild and rightchild. Each of those nodes also has two child nodes, and so on, until we reach the leaf nodes, nodes that have no children. Each node stores one nonnegative integer. The value at every non-leaf node is supposed to be the sum of its two children. But the value at one node is incorrect. Find this node.
+
+// Example: Consider the tree below. Note that 13 is the sum of 6 and 7, but the value 15 is incorrect, because 15 != 5 + 9 and 27 != 13 + 15. Clearly the 15 should be changed to 14.
+
+//            27
+//          /    \
+//         13    15
+//        /  \   / \
+//       6   7  5   9
+// The tree will always be full (all non-leaf nodes have exactly 2 children) and perfect (all leaves are on the bottom level). If we consider the root as level 1, its children as level 2, their children as level 3, and so on, the tree will contain at least 3 levels (and no more than 10). If the incorrect value occurs on the leaf level, then the right child will always be the incorrect one.
+
+// The tree is provided as a list or array (as appropriate to the language), with the nodes in breath-first order. This means the root is element 0, the root's leftchild is element 1, the root's rightchild is element 2, the root's leftchild's leftchild is element 3 and its rightchild is element 4, the root's rightchild's leftchild is element 5 and its rightchild is element 6, and so on.
+
+// Your function should return the index of the incorrect node and the value that it should be changed to. In a language that doesn't allow multiple values to be returned, return them in an array of two integers.
+
+// Examples:
+
+// If the tree is [27, 13, 15, 6, 7, 5, 9], the function should return 2,14.
+
+// If the tree is [21, 9, 10, 4, 5, 4, 6, 2, 2, 1, 4, 1, 3, 2, 4], the function should return 0,19.
+
+// If the tree is [29, 13, 16, 5, 8, 9, 1], the function should return 6,7. (This is because of the condition "if the incorrect value occurs on the leaf level, then the right child will always be the incorrect one." Since 9 + 1 != 16, the 1 must be changed to 7, rather than changing the 9 to 15.)
+
+// SOURCE: Programming Competition at the Midwest Instructional Computing Symposium, 2018.
+
+// If you like this kata, check out the harder binary tree problems, like Complete Binary Tree and Sort binary tree by levels .
+// function findIncorrectValue(tree) {
+//     for(let index=0;index<tree.length;index++){
+//       let leftChildIndex=1+2*index;
+//       let rightChildIndex=2+2*index;
+//       //console.log(`${tree} ${tree[index]} ${tree[leftChildIndex]} ${tree[rightChildIndex]}`)
+//       if(!checkChildren(tree[index],tree[leftChildIndex],tree[rightChildIndex])){
+//         //check if children are leafs and then check left child then right child. if both pass then root must be changed.
+//         if(childIsLeaf(rightChildIndex,tree)){
+//           return [rightChildIndex,tree[index]-tree[leftChildIndex]];
+//         }else if(!checkChildren(tree[leftChildIndex],tree[1+2*leftChildIndex],tree[2+2*leftChildIndex])){
+//           return [leftChildIndex,tree[index]-tree[rightChildIndex]];
+//         }else if(!checkChildren(tree[rightChildIndex],tree[1+2*rightChildIndex],tree[2+2*rightChildIndex])){
+//           return [rightChildIndex,tree[index]-tree[leftChildIndex]];
+//         }else{
+//           return [index,tree[leftChildIndex]+tree[rightChildIndex]];
+//         }
+//       }
+//     }
+//     return "No wrong nodes....or broken code";//Wow I had this in my for loop....
+//   }
+  
+//   function checkChildren(parent,left,right){
+//     if((left+right)==parent){
+//       return true;
+//     }else{
+//       return false;
+//     }
+//   }
+  
+//   function childIsLeaf(leafIndex,tree){
+//       console.log(tree[leafIndex] +" "+tree[2+2*leafIndex])
+//       if(tree[2+2*leafIndex]==undefined){
+//         return true;
+//       }else{
+//         return false;
+//       }
+//   }
