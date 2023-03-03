d1a186b023a45d7a1529a0a1969b2b10a160220e
CallMeAL <alexpgarner@gmail.com>
2023-03-02 23:40:39 -0800

    5kyu convert seconds to time problem

--- 5kyu/main.js
+++ 5kyu/main.js
@@ -14,3 +14,19 @@
 //   }
 
 // saw a clever answer using filter and then comparing array lengths to add zeros
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+// function humanReadable (seconds) {
+//     let hrs = (""+Math.floor(seconds/3600)).length<2 ? "0"+Math.floor(seconds/3600) : ""+Math.floor(seconds/3600) ;
+//     let min = (""+Math.floor(seconds%3600/60)).length<2 ? "0"+Math.floor(seconds%3600/60) : ""+Math.floor(seconds%3600/60);
+//     let sec = (""+Math.floor(seconds%3600%60)).length<2 ? "0"+Math.floor(seconds%3600%60) : ""+Math.floor(seconds%3600%60);
+//     return `${hrs}:${min}:${sec}`
+//   }
