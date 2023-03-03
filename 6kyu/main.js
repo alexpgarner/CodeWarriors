eadf56679e9f75f8ca94b13dbf402f0fe11893df
CallMeAL <alexpgarner@gmail.com>
2023-03-02 23:01:42 -0800

    6kyu recursive problem

--- 6kyu/main.js
+++ 6kyu/main.js
@@ -511,3 +511,18 @@
 //       return p + Math.pow(c, ('' + value).length)
 //       }, 0) == value;
 //   }
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// function digitalRoot(n) {
+//     let numArr = [...String(n)];
+//     if(numArr.length>1){
+//       let sum = numArr.reduce((accumulator,currentVal)=>accumulator+Number(currentVal),0);
+//       console.log(numArr,sum)
+//       return digitalRoot(sum)
+//     }else{
+//       return n;
+//     }
+//   }
